894bceceee73895cf8c3eabe5a5da724099fce88
Bimochan Shrestha <bmochan@gmail.com>
2019-07-29 13:07:31 +0545

    Create a default export and name change

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import { createBrowserHistory } from 'history';
 const broadcastChannel = new BroadcastChannel();
 
 /**
- * Call this on logout function
- */
-export const multiTabLogoutHandler = broadcastChannel.postMessage();
-
-/**
  * Call this in componentDidMount of root component. Probably App.js
  * @param {string} logoutPath  relative path to be redirected to. e.g: /login
  */
 export const logoutAllTabsListener = (logoutPath) => {
-		createBrowserHistory.push(logoutPath);
-		broadcastChannel.close();
-	};
+	createBrowserHistory.push(logoutPath);
+	broadcastChannel.close();
+};
+
+/**
+ * Call this on logout function
+ */
+export default const logoutHandler = broadcastChannel.postMessage();
