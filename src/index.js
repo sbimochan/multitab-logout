f4cdd9b40230356b718be6c4108ea25f71dde655
Bimochan Shrestha <bmochan@gmail.com>
2019-07-29 13:42:55 +0545

    Refresh page

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import BroadcastChannel from 'broadcast-channel';
-import { createBrowserHistory } from 'history';
 
 const broadcastChannel = new BroadcastChannel();
+
+/**
+ * Call this on logout function
+ */
+export const logoutHandler = () => broadcastChannel.postMessage();
 
 /**
  * Call this in componentDidMount of root component. Probably App.js
  * @param {string} logoutPath  relative path to be redirected to. e.g: /login
  */
-export const logoutAllTabsListener = (logoutPath) => {
-	createBrowserHistory.push(logoutPath);
-	broadcastChannel.close();
+export const logoutAllTabsListener = () => {
+	broadcastChannel.onmessage = event => {
+		window.location.reload()
+		broadcastChannel.close();
+	}
 };
-
-/**
- * Call this on logout function
- */
-export default const logoutHandler = broadcastChannel.postMessage();
